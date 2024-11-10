d6c7659e330d9a5298a09ac0f9bfc3e7f3b94c53
Md. Al-Amin <mdalaminh052@gmail.com>
2024-11-10 16:34:05 +0600

    redux set with axiosbasequery

--- src/lib/Providers/Providers.tsx
+++ src/lib/Providers/Providers.tsx
@@ -16,7 +16,7 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
     </Provider>
   );
 };
---
+++ src/types/common.ts
@@ -0,0 +1,5 @@
+export type Imeta = {
+  page: number;
+  limit: number;
+  total: number;
+};
