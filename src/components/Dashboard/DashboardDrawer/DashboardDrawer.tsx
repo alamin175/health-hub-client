0b66c0b8833d16f2cf67011c632692dc6330190c
Md. Al-Amin <mdalaminh052@gmail.com>
2024-11-10 23:46:50 +0600

    dashboard customized

--- src/components/Dashboard/DashboardDrawer/DashboardDrawer.tsx
+++ src/components/Dashboard/DashboardDrawer/DashboardDrawer.tsx
@@ -5,20 +5,14 @@ import Box from "@mui/material/Box";
 import MuiDrawer from "@mui/material/Drawer";
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
 import CssBaseline from "@mui/material/CssBaseline";
 import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+import DrawerMenu from "../Sidebar/Sidebar";
+import Link from "next/link";
 
 const drawerWidth = 240;
 
@@ -48,7 +42,6 @@ const DrawerHeader = styled("div")(({ theme }) => ({
   alignItems: "center",
   justifyContent: "flex-end",
   padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
   ...theme.mixins.toolbar,
 }));
 
@@ -130,12 +123,7 @@ export default function DashboardDrawer({
             aria-label="open drawer"
             onClick={handleDrawerOpen}
             edge="start"
-            sx={[
-              {
-                marginRight: 5,
-              },
-              open && { display: "none" },
-            ]}
+            sx={[{ marginRight: 5 }, open && { display: "none" }]}
           >
             <MenuIcon />
           </IconButton>
@@ -145,7 +133,27 @@ export default function DashboardDrawer({
         </Toolbar>
       </AppBar>
       <Drawer variant="permanent" open={open}>
-        <DrawerHeader>
+        <DrawerHeader
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Link href="/">
+            <Typography
+              variant="h5"
+              marginLeft="13px"
+              color="primary"
+              fontWeight={600}
+              fontSize={30}
+            >
+              Health
+              <Box component="span" color="secondary.dark" fontWeight={600}>
+                Hub
+              </Box>
+            </Typography>
+          </Link>
           <IconButton onClick={handleDrawerClose}>
             {theme.direction === "rtl" ? (
               <ChevronRightIcon />
@@ -154,110 +162,7 @@ export default function DashboardDrawer({
             )}
           </IconButton>
         </DrawerHeader>
-        <Divider />
-        <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: "block" }}>
-              <ListItemButton
-                sx={[
-                  {
-                    minHeight: 48,
-                    px: 2.5,
-                  },
-                  open
-                    ? {
-                        justifyContent: "initial",
-                      }
-                    : {
-                        justifyContent: "center",
-                      },
-                ]}
-              >
-                <ListItemIcon
-                  sx={[
-                    {
-                      minWidth: 0,
-                      justifyContent: "center",
-                    },
-                    open
-                      ? {
-                          mr: 3,
-                        }
-                      : {
-                          mr: "auto",
-                        },
-                  ]}
-                >
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText
-                  primary={text}
-                  sx={[
-                    open
-                      ? {
-                          opacity: 1,
-                        }
-                      : {
-                          opacity: 0,
-                        },
-                  ]}
-                />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: "block" }}>
-              <ListItemButton
-                sx={[
-                  {
-                    minHeight: 48,
-                    px: 2.5,
-                  },
-                  open
-                    ? {
-                        justifyContent: "initial",
-                      }
-                    : {
-                        justifyContent: "center",
-                      },
-                ]}
-              >
-                <ListItemIcon
-                  sx={[
-                    {
-                      minWidth: 0,
-                      justifyContent: "center",
-                    },
-                    open
-                      ? {
-                          mr: 3,
-                        }
-                      : {
-                          mr: "auto",
-                        },
-                  ]}
-                >
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText
-                  primary={text}
-                  sx={[
-                    open
-                      ? {
-                          opacity: 1,
-                        }
-                      : {
-                          opacity: 0,
-                        },
-                  ]}
-                />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
+        <DrawerMenu open={open} /> {/* Render the DrawerMenu component here */}
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <DrawerHeader />
---
+++ src/components/Dashboard/Sidebar/Sidebar.tsx
@@ -0,0 +1,68 @@
+// DrawerMenu.tsx
+import React from "react";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Tooltip from "@mui/material/Tooltip";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+
+interface DrawerMenuProps {
+  open: boolean;
+}
+
+const DrawerMenu: React.FC<DrawerMenuProps> = ({ open }) => {
+  const menuItems = [
+    { text: "Inbox", icon: <InboxIcon /> },
+    { text: "Starred", icon: <MailIcon /> },
+    { text: "Send email", icon: <InboxIcon /> },
+    { text: "Drafts", icon: <MailIcon /> },
+    { text: "All mail", icon: <InboxIcon /> },
+    { text: "Trash", icon: <MailIcon /> },
+    { text: "Spam", icon: <InboxIcon /> },
+  ];
+
+  return (
+    <>
+      <List>
+        {menuItems.map((item, index) => (
+          <ListItem key={item.text} disablePadding sx={{ display: "block" }}>
+            <Tooltip
+              title={item.text}
+              placement="right"
+              disableHoverListener={open}
+            >
+              <ListItemButton
+                sx={[
+                  { minHeight: 48, px: 2.5 },
+                  open
+                    ? { justifyContent: "initial" }
+                    : { justifyContent: "center" },
+                ]}
+              >
+                <ListItemIcon
+                  sx={[
+                    { minWidth: 0, justifyContent: "center" },
+                    open ? { mr: 3 } : { mr: "auto" },
+                  ]}
+                >
+                  {item.icon}
+                </ListItemIcon>
+                <ListItemText
+                  primary={item.text}
+                  sx={[open ? { opacity: 1 } : { opacity: 0 }]}
+                />
+              </ListItemButton>
+            </Tooltip>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </>
+  );
+};
+
+export default DrawerMenu;
